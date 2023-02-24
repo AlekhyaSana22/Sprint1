8c07f05f7b167f082cad8c5d48c3b6e4d318f131
alsana <alsana@INLEN8520048056.corp.capgemini.com>
2023-02-24 19:36:56 +0530

    Booking

--- src/main/java/com/capg/MultiplexBookingSystemApplication.java
+++ src/main/java/com/capg/MultiplexBookingSystemApplication.java
@@ -4,13 +4,20 @@ import org.springframework.boot.SpringApplication;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@RestController
 
 public class MultiplexBookingSystemApplication {
+		@GetMapping("/message")
+		public String getMessage() {
+			return "Welcome to MultiplexBookingSystem";
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiplexBookingSystemApplication.class, args);
